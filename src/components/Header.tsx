180efaa8ee9dcfc2ee31da49c53ee11e6f618239
dtumen <dt1109@mail.ru>
2024-04-02 13:38:50 +0200

    add Header.tsx, Navbar.tsx, Profile.tsx components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className='header'>
+    <img src="https://images.pexels.com/photos/1510150/pexels-photo-1510150.jpeg?auto=compress&cs=tinysrgb&w=800"
+      alt="Lorem img" />
+  </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div className='content'>
+        <div>
+          <img
+            src="https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="nature"/>
+        </div>
+        <div>ava + desc</div>
+
+        <div>
+          My posts
+          <div>New Post</div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+            <div>Post 3</div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Profile;
